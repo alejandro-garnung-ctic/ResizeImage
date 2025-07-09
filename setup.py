fd4b472f07822141b2597195ed32d11bdd82568b
Alejandro Garnung Menéndez <alejandro.garnung@fundacionctic.org>
2025-07-09 14:08:57 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="comfyui-resize-node",
+    version="0.0.1",
+    description="Nodo de redimensionamiento de imágenes para ComfyUI",
+    author="Alejandro Garnung Menéndez",
+    license="",
+    packages=find_packages(),
+)
